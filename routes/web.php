237160b33b08550ce4f07f66159f37ca5e376071
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-09-04 11:17:28 +0700

    Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,32 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
+
+
+
+// Praktikum 2
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+   ]);
+   Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+   ]);
+   
+Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/', [WelcomeController::class,'index']);
+Route::get('/about', [WelcomeController::class,'about']);
+Route::get('/articles/{Id}', [WelcomeController::class,'articles']);
+
+Route::get('/hello', [HomeController::class,'hello']);
+Route::get('/', [AboutController::class,'index']);
+Route::get('/about', [ArticleController::class,'about']);
+Route::get('/articles/{Id}', [WelcomeController::class,'articles']);
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +39,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Praktikum 1
+Route::get('/welcome', function () {
+    return 'Selamat Datang Viesta';
 });
+
+
+Route::get('/NIM', function () {
+ return '2241760126 Muhammad Toyfur';
+});
+
+Route::get('/user/{viesta}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+
+// Route::get('/articles/{id}', function
+   // ($id) {
+     //return 'Halaman Artikel dengan ID' .$id;
+    //});
+
+ Route::get('/user/{name?}', function ($name=null) {
+        return 'Nama saya Rizka '.$name;
+});
+
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });

bdfa76c2734142f1127ca20a71493518307f3667
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-09-04 11:17:49 +0700

    Praktikum 2

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class WelcomeController extends Controller
+{
+public function hello() {
+ return "Hello World";
+}
+public function index() {
+    return "Selamat Datang";
+   }
+   public function about() {
+    return 'Muhammad Toyfur, 2241760126';
+   }
+   public function articles($Id) {
+    return 'Halaman Ke'.$Id;
+   }
+
+}
+
